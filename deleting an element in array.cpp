a65a3483f0584e5b3a004b748ff96a1edfec94b0
Shah Haamid <80157961+Hames-1616@users.noreply.github.com>
2022-06-07 22:49:57 +0530

    update

--- deleting an element in array.cpp
+++ deleting an element in array.cpp
@@ -1,1 +1,34 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    int arr[8];
+    cout<<"Enter the elements"<<endl;
+    for(int i=0;i<8;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Enter the element you want to delete"<<endl;
+    cin>>x;
+    for(int i=0;i<8;i++)
+    {
+        if(arr[i]==x)
+        {
+            arr[i]=-1;
+            break;
+        }
+        else
+        {
+            cout<<"Element not found"<<endl;
+        }
+    }
+    cout<<endl;
+    for(int i=0;i<8;i++)
+    {
+        if(arr[i]!=-1)
+        {
+            cout<<arr[i]<<endl;
+        }
+    }
+}
