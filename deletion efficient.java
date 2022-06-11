249d69e343019e5ed65645f2c9cb9a9869e93b6a
Shah Haamid <80157961+Hames-1616@users.noreply.github.com>
2022-06-11 00:20:53 -0700

    upload

--- deletion efficient.java
+++ deletion efficient.java
@@ -4,12 +4,37 @@ public class Delete
     public static void main(String a[])
     {
         int x,n;
+        System.out.println("enter the elements that you want to enter");
+        Scanner s = new Scanner(System.in);
+        n=s.nextInt();
         int[] arr = new int[n];
-        cout<<"enter the elements that you want to enter"<<endl;
-        cin>>n;
         for(int i=0;i<n;i++)
         {
-            cin>>
+            arr[i]=s.nextInt();
+        }
+        System.out.println("enter the number that you want to delete");
+        x=s.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==x)
+            {
+                System.out.println("element Found");
+                for(i;i<n;i++)
+                {
+                    arr[i]=arr[i+1];
+                }
+                break;
+            }
+            else
+            {
+                System.out.println("Element not found");
+            }
+        }
+        n=n-1;
+        System.out.println("Printing elements%n");
+        for(int i=0;i<n;i++)
+        {
+
         }
     }
 }
