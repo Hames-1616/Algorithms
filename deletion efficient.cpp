8d905af873c7db2946e30fb1447f6897264f789a
Shah Haamid <80157961+Hames-1616@users.noreply.github.com>
2022-06-16 01:40:09 +0530

    upload

--- deletion efficient.cpp
+++ deletion efficient.cpp
@@ -1,10 +1,28 @@
 #include<iostream>
 using namespace std;
+void efficient(int a[],int s)
+{
+    int c[s];
+    for(int i=0;i<s;i++)
+    {
+        c[i]=a[i];
+        cout<<c[i]<<endl;
+    }
+}
 
 int main()
 {
-    int a[]={1,2,3,2,4,2,8};
-    int size=7,x=2;
+    int size;
+    cout<<"enter the number of elements that you want to enter"<<endl;
+    cin>>size;
+    int a[size];
+    for(int i=0;i<size;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<endl<<"enter the element that you want to delete"<<endl;
+    int x;
+    cin>>x;
     int j=0,n=0,b[n];
     for(int i=0;i<size;i++)
     {
@@ -17,10 +35,5 @@ int main()
         }
         
     }
-    
-    for(int i=0;i<n;i++)
-    {
-        
-        cout<<b[i]<<endl;
-    }
+    efficient(b,n); 
 }
